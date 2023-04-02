affdded2ee56c2fafe4ca7640aee55a4d479a626
Nicolás Órdenes <nicordenes@gmail.com>
2023-04-02 17:37:11 -0400

    fix: ots beign created with nil client names

--- lib/bpglass.rb
+++ lib/bpglass.rb
@@ -5,4 +5,8 @@ require_relative "bpglass/ot"
 require_relative "bpglass/posicion"
 
 module BPGlass
+  def self.load_files(dir_path = "./Archivos AW")
+    filepaths = Dir.glob("#{dir_path}/*.xlsx")
+    filepaths.map { |filepath| OT.from_xlsx(filepath) }
+  end
 end
---
+++ spec/bpglass/bpglass_spec.rb
@@ -0,0 +1,19 @@
+describe BPGlass do
+  describe "#load_files" do
+    let(:file_path) { "./spec/fixtures/Archivos AW" }
+    it "loads all the files the target directory" do
+      ots = BPGlass.load_files(file_path)
+      expect(ots.count).to eq(30)
+    end
+  end
+
+  context "sanity checks" do
+    let(:ots) { BPGlass.load_files.sort_by!(&:id) }
+    it "all OTs have a cliente" do
+      ots.each do |ot|
+        p [ot.id, ot.cliente]
+      end
+      expect(ots.map(&:cliente).none?(nil)).to eq(true)
+    end
+  end
+end
