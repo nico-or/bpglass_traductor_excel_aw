61dfa051201e346272f9c2ae2d5bad5dfbf116f4
Nicolás Órdenes <nicordenes@gmail.com>
2023-04-02 19:19:33 -0400

    add clear to lookup hashes

--- lib/bpglass/constants.rb
+++ lib/bpglass/constants.rb
@@ -1,6 +1,6 @@
 module BPGlass
   ID_VIDRIO = {
-    100 => "100 - Incoloro 04 mm",
+    100 => "Incoloro 04 mm",
     260 => "Saten 04 mm",
     350 => "Laminado 3+3.1 Inc",
 
@@ -9,11 +9,13 @@ module BPGlass
   ESPECIAL_ID = {
     "saten" => [260],
     "laminado" => [350],
+    "clear" => [280],
   }
 
   ESPECIAL_ALIAS = {
     "saten" => "SAT",
     "laminado" => "LAM",
+    "clear" => "CLEAR",
   }
 
   CLIENTE_ALIAS = {
