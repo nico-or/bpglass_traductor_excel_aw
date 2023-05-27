249e18f24741d0d20dbc0417de11ddc09abd6db8
Nicolás Órdenes <nicordenes@gmail.com>
2023-05-27 17:44:38 -0400

    test: skip slow tests

--- spec/bpglass/bpglass_spec.rb
+++ spec/bpglass/bpglass_spec.rb
@@ -1,7 +1,7 @@
 describe BPGlass do
   describe "#load_files" do
     let(:file_path) { "./spec/fixtures/Archivos AW" }
-    it "loads all the files the target directory" do
+    xit "loads all the files the target directory" do
       ots = BPGlass.load_files(file_path)
       expect(ots.count).to eq(30)
     end
@@ -9,7 +9,7 @@ describe BPGlass do
 
   context "sanity checks" do
     let(:ots) { BPGlass.load_files.sort_by!(&:id) }
-    it "all OTs have a cliente" do
+    xit "all OTs have a cliente" do
       expect(ots.map(&:cliente).none?(nil)).to eq(true)
     end
   end
