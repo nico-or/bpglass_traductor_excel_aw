ede98f5cd9573d998b20859552fb63033b0437f9
Nicolás Órdenes <nicordenes@gmail.com>
2023-06-11 17:39:38 -0400

    refactor: add string support for Cristales.[]

--- lib/bpglass/cristales.rb
+++ lib/bpglass/cristales.rb
@@ -2,12 +2,17 @@ require "yaml"
 
 module BPGlass
   module Cristales
-    Cristal = Struct.new("Cristal", :id, :cotizador, :especial, :short_name, keyword_init: true)
+    Cristal = Struct.new("Cristal", :id, :alias, :especial, :short_name, keyword_init: true)
 
     DB = YAML.load_file("./lib/data/cristales.yml").map { Cristal.new(_1) }
 
-    def self.[](cristal_id)
-      DB.find { |cristal| cristal.id.eql?(cristal_id) }
+    def self.[](query)
+      cristal = DB.find { |cristal| cristal.id.eql?(query) } ||
+                DB.find { |cristal| cristal.alias.include?(query) }
+
+      raise ArgumentError.new("Unsupported Cristal: #{query}") unless cristal
+
+      cristal
     end
   end
 end
--- spec/bpglass/cristales_spec.rb
+++ spec/bpglass/cristales_spec.rb
@@ -1,10 +1,34 @@
 describe BPGlass::Cristales do
   describe ".[]" do
-    it " returns a Cristal object" do
-      id = 350
+    it "parses the BPGlass nomenclature" do
+      id = "280 - Ecl. Adv . Clear 06 mm"
       cristal = BPGlass::Cristales[id]
 
-      expect(cristal.class).to eq(Struct::Cristal)
+      expect(cristal.id).to eq(280)
+    end
+
+    it "parses Bastro nomenclature" do
+      id = "INC 4"
+      cristal = BPGlass::Cristales[id]
+
+      expect(cristal.id).to eq(100)
+    end
+
+    it "it returns the Cristal when quering with an integer" do
+      id = 130
+      cristal = BPGlass::Cristales[id]
+
+      expect(cristal.id).to eq(130)
+    end
+
+    it "raises an ArgumentError when a string query has no match" do
+      id = "foo"
+      expect { BPGlass::Cristales[id] }.to raise_error(ArgumentError)
+    end
+
+    it "raises an ArgumentError when an integer query has no match" do
+      id = 999
+      expect { BPGlass::Cristales[id] }.to raise_error(ArgumentError)
     end
   end
 end
