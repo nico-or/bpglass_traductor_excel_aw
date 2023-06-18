98a74f975e0edff4f3f1fb54c2310560b1013680
Nicolás Órdenes <nicordenes@gmail.com>
2023-06-18 17:38:34 -0400

    refactor(main): add exception handling
    add exception handling and additional filename
    filtering to avoid a common crash scenarios (windows hidden files)

--- main.rb
+++ main.rb
@@ -1,18 +1,32 @@
 #! /bin/ruby
 
 require "csv"
+require "logger"
 require_relative "lib/bpglass"
 
-BASE_PATH = Pathname.new("./Archivos AW")
-aw_files = Dir.children(BASE_PATH).map { |xlsx_file| BASE_PATH.join(xlsx_file) }.sort!
+DEFAULT_DIR_PATH = Pathname.new("./Archivos AW")
 
+# matches filenames like 30180-AW-27-06-2023.xlsx
+BPGLASS_FILENAME_REGEX = /\d{5}-AW-\d{2}-\d{2}-\d{4}\.xlsx/
+
+AW_FILENAMES = Dir.children(DEFAULT_DIR_PATH)
+  .select { |filename| filename.match? BPGLASS_FILENAME_REGEX }
+  .map { |filename| DEFAULT_DIR_PATH.join(filename) }.sort!
+
+# file where OT output will be written
 OUTPUT_FILENAME = "out.txt"
+
 OUTPUT_FILE = CSV.open(OUTPUT_FILENAME, "w", col_sep: "\t") do |output_file|
-  aw_files.each do |xlsx|
-    puts "Processing: #{xlsx}"
-    ot = BPGlass::OT.from_excel_import(xlsx)
-    ot.output_arrays.each do |array|
-      output_file << array
+  AW_FILENAMES.each do |aw_file|
+    begin
+      puts "Processing: #{aw_file}"
+      ot = BPGlass::OT.from_excel_import(aw_file)
+      ot.output_arrays.each do |array|
+        output_file << array
+      end
+    rescue => exception
+      puts exception
+      puts "Error while processing #{aw_file}"
     end
   end
 end
