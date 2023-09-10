56a2ea1d761fbda1dac9d44d3a6cb4cdbbc6c74a
Nicolás Órdenes <nicordenes@gmail.com>
2023-09-10 11:34:02 -0300

    feat: Add CargaFuturaWriter CsvWriter

---
+++ carga_futura.rb
@@ -0,0 +1,29 @@
+#! /bin/ruby
+
+require_relative "lib/bpglass"
+
+DEFAULT_DIR_PATH = Pathname.new("./Archivos AW")
+
+# matches filenames like 30180-AW-27-06-2023.xlsx
+BPGLASS_FILENAME_REGEX = /\d{5}-AW-\d{2}-\d{2}-\d{4}\.xlsx/
+
+AW_FILENAMES = Dir.children(DEFAULT_DIR_PATH)
+  .select { |filename| filename.match? BPGLASS_FILENAME_REGEX }
+  .map { |filename| DEFAULT_DIR_PATH.join(filename) }.sort
+
+# file where OT output will be written
+OUTPUT_FILENAME = "carga_futura.txt"
+
+File.open(OUTPUT_FILENAME, "w") do |output_file|
+  AW_FILENAMES.each do |aw_file|
+    puts "Processing: #{aw_file}"
+    begin
+      ot = BPGlass::OT.from_excel_import(aw_file)
+      writer = BPGlass::CsvWriter::CargaFuturaWriter.new(ot)
+      output_file.write(writer.to_csv)
+    rescue => exception
+      puts "Error while processing: #{aw_file}"
+      puts exception
+    end
+  end
+end
--- lib/bpglass/csv_writer.rb
+++ lib/bpglass/csv_writer.rb
@@ -2,6 +2,7 @@ require "csv"
 
 require_relative "csv_writer/planificacion_writer"
 require_relative "csv_writer/carga_masiva_writer"
+require_relative "csv_writer/carga_futura_writer"
 
 module BPGlass
   module CsvWriter
